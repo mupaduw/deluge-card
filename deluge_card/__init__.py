bf766dcb7ebbe3c33dd666ade2a5aa92d6ea2699
Chris Chamberlain <chrisbc@artisan.co.nz>
2022-05-03 17:55:50 +1200

    Bump version: 0.2.3 → 0.3.0

--- deluge_card/__init__.py
+++ deluge_card/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Chris Chamberlain"""
 __email__ = 'chrisbc@artisan.co.nz'
-__version__ = '0.2.3'
+__version__ = '0.3.0'
 
 from .deluge_card import DelugeCardFS, InvalidDelugeCard, list_deluge_fs
 from .deluge_song import DelugeSong
