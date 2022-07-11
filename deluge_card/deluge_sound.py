2988c549da3d6f4e9d784e6a7301f9a7bfdd5b43
Chris Chamberlain <chrisbc@artisan.co.nz>
2022-07-11 17:24:49 +1200

    more WIP

--- deluge_card/deluge_sound.py
+++ deluge_card/deluge_sound.py
@@ -9,7 +9,7 @@ from .deluge_synth import DelugeSynth
 
 
 class Polyphony(enum.Enum):
-    """Enumerate the available synth polyphony modes."""
+    """Enumerate the synth polyphony modes."""
 
     # name = value
     polyphonic = 'poly'
@@ -18,51 +18,104 @@ class Polyphony(enum.Enum):
 
 
 class SynthMode(enum.Enum):
-    """Enumerate the available synth engine modes."""
+    """Enumerate the synth engine modes."""
 
     subtractive = 'subtractive'
 
 
 class LpfMode(enum.Enum):
-    """Enumerate the available synth engine modes."""
+    """Enumerate the Low-pass filter modes."""
 
     _12dB = '12dB'
     _24dB = '24dB'
 
 
+class ModFxType(enum.Enum):
+    """Enumerate the Mod FX options."""
+
+    none = 'none'
+    phaser = 'phaser'
+
+
 def attr_or_elem(elem: lxml.etree._Element, name: str, cast=str):
     """Deluge Sytth/Sound are similar forms but the former uses elements and the latter uses attributes."""
-    if elem.get(name):
-        rval = elem.get(name)
-    else:
-        rval = elem.getroottree().find(f'.//{name}').text
-    print(name, rval, cast)
+    rval = None
+    try:
+        if elem.get(name):
+            rval = elem.get(name)
+        else:
+            rval = elem.getroottree().find(f'.//{name}').text
+    except Exception as err:
+        print('got exception', name, rval, cast)
+        raise err
     return cast(rval)
 
 
+class Base(object):
+    """https://stackoverflow.com/a/59987363."""
+
+    def __post_init__(self):
+        # just intercept the __post_init__ calls so they
+        # aren't relayed to `object`
+        pass
+
+
 @dataclass
-class BaseSound:
+class NamedSound(Base):
+    """A sound base class."""
+
+    name: str = field(init=False)
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.name = attr_or_elem(self.sound, 'name')
+        # print('NamedSound.__post_init__ named:', self.name)
+
+
+@dataclass
+class PolyphonicSound(Base):
+    """A polyphonic composable class."""
+
+    polyphonic: Polyphony = field(init=False)
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.polyphonic = Polyphony(attr_or_elem(self.sound, 'polyphonic'))
+
+
+@dataclass
+class PresetSound(Base):
+    """A preset composable class."""
+
+    preset_slot: int = field(init=False)
+    preset_sub_slot: int = field(init=False)
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.preset_slot = attr_or_elem(self.sound, 'presetSlot')
+        self.preset_sub_slot = attr_or_elem(self.sound, 'presetSubSlot')
+
+
+@dataclass
+class BaseSound(Base):
     """A sound base class."""
 
     sound: lxml.etree._Element
-    name: str = field(init=False)
     lpf_mode: LpfMode = field(init=False)
     mode: SynthMode = field(init=False)
     mod_fx_type: str = field(init=False)
-    polyphonic: Polyphony = field(init=False)
     voice_priority: int = field(init=False)
 
     def __post_init__(self):
-        self.name = self.sound.get('name')
-        self.polyphonic = Polyphony(attr_or_elem(self.sound, 'polyphonic'))
+        super().__post_init__()
         self.mode = SynthMode(attr_or_elem(self.sound, 'mode'))
         self.lpf_mode = LpfMode(attr_or_elem(self.sound, 'lpfMode'))
-        self.mod_fx_type = attr_or_elem(self.sound, 'modFXType')
+        self.mod_fx_type = ModFxType(attr_or_elem(self.sound, 'modFXType'))
         self.voice_priority = attr_or_elem(self.sound, 'voicePriority', int)
 
 
 @dataclass
-class DelugeSynthSound(BaseSound):
+class DelugeSynthSound(BaseSound, PolyphonicSound):
     """A synth sound."""
 
     transpose: int = field(init=False)
@@ -70,7 +123,6 @@ class DelugeSynthSound(BaseSound):
     def __post_init__(self):
         super().__post_init__()
         self.transpose = int(self.sound.getroottree().find('./transpose').text)
-        print("transpose", self.transpose)
 
     @staticmethod
     def from_synth(synth: DelugeSynth) -> 'DelugeSynthSound':
@@ -82,34 +134,25 @@ class DelugeSynthSound(BaseSound):
 
 
 @dataclass
-class DelugeSongSound(BaseSound):
-    """A song instrument sound."""
+class DelugeSongSound(BaseSound, PolyphonicSound, PresetSound):
+    """A song instrument synth sound."""
 
     # activeModFunction: bool = field(init=False)
-    defaultVelocity: int = field(init=False)
-    isArmedForRecording: bool = field(init=False)
-    presetSlot: int = field(init=False)
-    presetSubSlot: int = field(init=False)
+    default_velocity: int = field(init=False)
+    is_armed: bool = field(init=False)
 
     def __post_init__(self):
         super().__post_init__()
         # self.activeModFunction = attr_or_elem(self.sound, 'activeModFunction', )
-        self.defaultVelocity = attr_or_elem(self.sound, 'defaultVelocity', int)
-        self.isArmedForRecording = attr_or_elem(self.sound, 'isArmedForRecording', bool)
-
-    # @staticmethod
-    # def from_song(song: 'DelugeSong') -> 'DelugeSongSound':
-    #     """Get a sy
-    #     # print(f'from_synth {synth.xmlroot} {synth.xmlroot.keys()} {synth.xmlroot.get("polyphonic")}
-    #       {dir(synth.xmlroot)}')
-    #     return DelugeSongSound(synth.xmlroot)
+        self.default_velocity = attr_or_elem(self.sound, 'defaultVelocity', int)
+        self.is_armed = attr_or_elem(self.sound, 'isArmedForRecording', bool)
 
 
 @dataclass
-class DelugeSongKitSound(BaseSound):
+class DelugeSongKitSound(BaseSound, NamedSound):
     """A song instrument sound."""
 
     # activeModFunction: bool = field(init=False)
-
     def __post_init__(self):
+        # NamedSound.__post_init__(self)
         super().__post_init__()
--- tests/test_deluge_sound.py
+++ tests/test_deluge_sound.py
@@ -5,8 +5,8 @@ from unittest import TestCase, mock
 
 from lxml import etree
 
-from deluge_card import DelugeCardFS, DelugeSynth
-from deluge_card.deluge_sound import DelugeSongSound, DelugeSynthSound, Polyphony, SynthMode
+from deluge_card import DelugeCardFS, DelugeSong, DelugeSynth
+from deluge_card.deluge_sound import DelugeSongSound, DelugeSynthSound, LpfMode, ModFxType, Polyphony, SynthMode
 
 
 class TestSoundFromSynth(TestCase):
@@ -30,7 +30,73 @@ class TestSoundFromSynth(TestCase):
         self.assertEqual(sound.mode.name, 'subtractive')
         self.assertEqual(sound.mode.value, 'subtractive')
 
-    def test_trsnpose(self):
+    def test_transpose(self):
         sound = DelugeSynthSound.from_synth(self.synth)
-        print(dir(sound))
         self.assertEqual(sound.transpose, -12)
+
+    def test_LPF_mode(self):
+        sound = DelugeSynthSound.from_synth(self.synth)
+        self.assertEqual(sound.lpf_mode.value, '24dB')
+
+
+class TestSongBase(TestCase):
+    def setUp(self):
+        cwd = os.path.dirname(os.path.realpath(__file__))
+        self.card = DelugeCardFS(Path(cwd, 'fixtures', 'DC01'))
+        p = Path(cwd, 'fixtures', 'DC01', 'SONGS', 'SONG001.XML')
+        self.song = DelugeSong(self.card, p)
+
+
+class TestSynthSoundsFromSong(TestSongBase):
+    def test_base_sound_attribs(self):
+        sound = list(self.song.synths)[0]
+        self.assertEqual(sound.polyphonic.value, 'poly')
+        self.assertEqual(sound.mode.value, 'subtractive')
+        self.assertEqual(sound.lpf_mode.value, '12dB')
+        print(sound, dir(sound))
+        self.assertEqual(sound.mod_fx_type, ModFxType.none)
+        self.assertEqual(sound.voice_priority, 1)
+
+    def test_song_sound_attribs(self):
+        sound = list(self.song.synths)[0]
+        self.assertEqual(sound.preset_slot, "133")
+        self.assertEqual(sound.preset_sub_slot, "-1")
+
+
+class TestKitSoundsFromSong(TestSongBase):
+    def setUp(self):
+        super().setUp()
+        self.kit = list(self.song.kits)[0]
+        self.sounds = list(self.kit.sounds)
+
+    def test_sound_synth_mode(self):
+        self.assertEqual(self.sounds[0].mode.value, 'subtractive')
+
+    def test_lpf_mode(self):
+        self.assertEqual(self.sounds[0].lpf_mode, LpfMode._24dB)
+        print(self.sounds[0])
+        print(self.sounds[0], dir(self.sounds[0]))
+
+    def test_mod_fx_type(self):
+        self.assertEqual(self.sounds[0].mod_fx_type, ModFxType.none)
+
+    def test_voice_priority(self):
+        self.assertEqual(self.sounds[0].voice_priority, 1)
+
+    # def test_song_sound_attribs(self):
+    #     kit = list(self.song.kits)[0]
+    #     sound = list(kit.sounds)[0]
+    #     self.assertEqual(kit.preset_slot, "133")
+    #     self.assertEqual(kit.preset_sub_slot, "-1")
+    #     self.assertEqual(kit.name, 'quail')
+
+    # def test_sound_mode(self):
+    #     sound = DelugeSynthSound.from_synth(self.synth)
+    #     self.assertEqual(sound.mode.name, SynthMode('subtractive').name)
+    #     self.assertEqual(sound.mode.name, 'subtractive')
+    #     self.assertEqual(sound.mode.value, 'subtractive')
+
+    # def test_trsnpose(self):
+    #     sound = DelugeSynthSound.from_synth(self.synth)
+    #     print(dir(sound))
+    #     self.assertEqual(sound.transpose, -12)
